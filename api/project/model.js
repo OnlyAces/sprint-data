03e79af0a8120a2a0b5b39db2ac530a4a9f4e4db
Isaac Henry <isaachenry751@gmail.com>
2024-03-03 17:19:00 -0500

    comm

---
+++ api/project/model.js
@@ -0,0 +1,16 @@
+// build your `Project` model here
+const db = require("../../data/dbConfig");
+
+function getProjects() {
+  return db("projects");
+}
+
+async function insert(project) {
+  const [project_id] = await db("projects").insert(project);
+  return db("projects").where({ project_id }).first();
+}
+
+module.exports = {
+  getProjects,
+  insert,
+};
---
+++ api/resource/router.js
@@ -0,0 +1,24 @@
+// build your `/api/resources` router here
+const express = require("express");
+
+const Resource = require("./model");
+
+const router = require("express").Router();
+
+router.get("/", (req, res, next) => {
+  Resource.getResources()
+    .then((resource) => {
+      res.status(200).json(resource);
+    })
+    .catch(next);
+});
+
+router.post("/", (req, res, next) => {
+  Resource.insert(req.body)
+    .then((resource) => {
+      res.status(200).json(resource);
+    })
+    .catch(next);
+});
+
+module.exports = router;
